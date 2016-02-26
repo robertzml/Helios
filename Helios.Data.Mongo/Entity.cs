19e6ed591d8d407d9dbe34aaa0edf39534468bed
robertzml <robertzml@126.com>
2016-02-26 16:50:46 +0800

    Add Helios.Data.Mongo project. Add MongoRepository class.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Helios.Data.Mongo/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Helios.Data.Mongo
+{
+    /// <summary>
+    /// Abstract Entity for all the BusinessEntities.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    [BsonIgnoreExtraElements(Inherited = true)]
+    public abstract class Entity : IEntity<string>
+    {
+        /// <summary>
+        /// Gets or sets the id for this object (the primary record for an entity).
+        /// </summary>
+        /// <value>The id for this object (the primary record for an entity).</value>
+        [DataMember]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public virtual string Id { get; set; }
+    }
+}
---
+++ Helios.Data.Mongo/IEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Helios.Data.Mongo
+{
+    /// <summary>
+    /// Generic Entity interface.
+    /// </summary>
+    /// <typeparam name="TKey">The type used for the entity's Id.</typeparam>
+    public interface IEntity<TKey>
+    {
+        /// <summary>
+        /// Gets or sets the Id of the Entity.
+        /// </summary>
+        /// <value>Id of the Entity.</value>
+        [BsonId]
+        TKey Id { get; set; }
+    }
+
+    /// <summary>
+    /// "Default" Entity interface.
+    /// </summary>
+    /// <remarks>Entities are assumed to use strings for Id's.</remarks>
+    public interface IEntity : IEntity<string>
+    {
+    }
+}
